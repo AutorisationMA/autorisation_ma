3cf7d2e1666c25049b79aefec290434f69646968
AutorisationMA <instances.import@gmail.com>
2025-09-30 13:15:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,7 @@ elif menu == "📥 MA Import" and st.session_state.role != "consult":
     st.subheader("Ajouter une nouvelle autorisation")
     matricule = st.text_input("Matricule").strip().upper()
 
-   declarants_liste = [
-    "1 PORT", "2HR TRANS", "2S2A EXPRESS", "3 C LOG SOLAR", "365 LOGISTIQUE", 
+   declarants_liste = ["1 PORT", "2HR TRANS", "2S2A EXPRESS", "3 C LOG SOLAR", "365 LOGISTIQUE", 
     "3S TRANSIT", "4G TRANS", "4TL MED TRANSIT", "A FIVE INTERNATIONAL", "A RIGHT TRANS"]
 
 declarant = st.selectbox("Déclarant", options=declarants_liste)
@@ -298,6 +297,7 @@ elif menu == "📊 Consulter MA":
     df_filtered = df_filtered.sort_values(by="Date_ajout", ascending=False)
 
     st.dataframe(df_filtered)
+
 
 
 
