7067c07e76e87c9f3f7be108d13981c515b6508d
AutorisationMA <instances.import@gmail.com>
2025-09-30 13:02:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ elif menu == "📥 MA Import" and st.session_state.role != "consult":
 
     declarants_liste = [ "","1 PORT", "2HR TRANS", "2S2A EXPRESS", "3 C LOG SOLAR", "365 LOGISTIQUE", "3S TRANSIT", "4G TRANS", "4TL MED TRANSIT", "A FIVE INTERNATIONAL", "A RIGHT TRANS"]
 
-     declarant = st.selectbox("Déclarant", options=declarants_liste)
+    declarant = st.selectbox("Déclarant", options=declarants_liste)
 
     ref = st.text_input("Référence_MA").strip().upper()
 
@@ -297,6 +297,7 @@ elif menu == "📊 Consulter MA":
     df_filtered = df_filtered.sort_values(by="Date_ajout", ascending=False)
 
     st.dataframe(df_filtered)
+
 
 
 
