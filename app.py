162ef0c43301b56abb45894c0105c7d3d8cefcb5
AutorisationMA <instances.import@gmail.com>
2025-10-05 01:53:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -358,8 +358,10 @@ except Exception as e:
 # --- Consultation / Export ---
 # ==========================
 
+# ==========================
+# --- Consultation / Export ---
+# ==========================
 elif menu == "📊 Consulter MA":
-
     st.subheader("Filtrer les autorisations MA")
 
     # Charger toutes les données depuis Supabase
@@ -474,8 +476,6 @@ elif menu == "📊 Consulter MA":
         df_recent = df.head(10)[["id", "Matricule", "Reference_MA", "Pays", "Date_ajout", "Exporte"]].copy()
         df_recent.columns = ["ID", "N°", "Réf. MA", "Pays", "Date", "Statut"]
         st.dataframe(df_recent, use_container_width=True)
-
-
 
 
 
