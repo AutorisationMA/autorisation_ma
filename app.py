9e6b542f16de3f0c9df0de5945df1a985bc35694
AutorisationMA <instances.import@gmail.com>
2025-10-05 01:50:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -357,6 +357,7 @@ except Exception as e:
 # ==========================
 # --- Consultation / Export ---
 # ==========================
+
 elif menu == "📊 Consulter MA":
     st.subheader("Filtrer les autorisations MA")
 
@@ -472,6 +473,7 @@ elif menu == "📊 Consulter MA":
         df_recent = df.head(10)[["id", "Matricule", "Reference_MA", "Pays", "Date_ajout", "Exporte"]].copy()
         df_recent.columns = ["ID", "N°", "Réf. MA", "Pays", "Date", "Statut"]
         st.dataframe(df_recent, use_container_width=True)
+
 
 
 
